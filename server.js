2ffb597bd04548f8faaff000fa60baa989eec5f3
TrammyFTW <freeflowers997@gmail.com>
2017-03-17 21:34:39 +0800

    Hi

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.set('port', (process.env.PORT || 5000));
 //use static files
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
+app.use('/public', express.static(__dirname + '/'));
 //express routes
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '/index.html'));
