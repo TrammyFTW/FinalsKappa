e84c26026c773653e5f14255fff5acedb3bea593
TrammyFTW <freeflowers997@gmail.com>
2017-03-18 17:20:23 +0800

    hi

--- app.js
+++ app.js
@@ -7,24 +7,14 @@ $.ajax({
 	console.log(res);
   var i;
 	let count = res.albums.total;
-	let html=`<div class="col s12 m7">
-    <div class="card horizontal">
-      <div class="card-image">
-        <img src="${res.albums.items[0].images[1].url}" style="width:150px" style="height:150px">
-      </div>
-      <div class="card-stacked">
-        <div class="card-content">
-          <p>${res.albums.items[0].name}</p>
-        </div>
-      </div>
-    </div>
-  </div>`;
+	let html=``;
 
-  for(i=1; i<count; i++){
-    html += `<div class="col s12 m7">
+  for(i=0; i<count; i++){
+    html += `
+    <div class="col s12 m7">
     <div class="card horizontal">
       <div class="card-image">
-        <img src="${res.albums.items[i].images[1].url}" style="width:150px" style="height:150px">
+        <img src="${res.albums.items[i].images[1].url}" style="width:100px" style="height:100px">
       </div>
      <div class="card-stacked">
        <div class="card-content">
@@ -41,6 +31,30 @@ $.ajax({
     url:"https://api.spotify.com/v1/albums/717ryge05ohaVKGzbNykeQ"
   }).done(function(album){
       console.log(album);
+      var j;
+      let count2 = album.tracks.items.length;
+      let html2 = `>`;
+
+      for(j=0; j<count2; j++){
+        html2 += `
+    <div class="col s12 m7">
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="${album.images[1].url}" style="width:100px" style="height:100px">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>${album.tracks.items[j].name}</p>
+          <p><audio controls>
+          <source src="${album.tracks.items[j].preview_url}" type="audio/ogg">
+          </audio>
+        </div>
+      </div>
+    </div>
+  </div>`;
+      }
+
+  document.querySelector('#lel').innerHTML = html2;
 });
 
 
